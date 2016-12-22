8f651f591e80cfd2ad9146be09caab09ae23f896
DanShafman <dshafman0900@bths.edu>
2016-12-22 13:46:07 -0500

    Extraneous files

--- src/kek.py
+++
@@ -1,6 +0,0 @@
-import requests as r
-import json
-
-apiKey = "AIzaSyDKL9iF-TujlZqANx3DSEkD-EpQ5XW5DNs"
-resp = r.get("https://maps.googleapis.com/maps/api/distancematrix/json?origins=Kings+County,+NY&destinations=Los+Angeles+County,+CA&key=%s" % apiKey)
-print(json.loads(resp.text)["rows"][0]["elements"][0]["distance"]["value"])
